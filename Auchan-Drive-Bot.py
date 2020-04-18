f9532930e5b3e3a5e9e78e128b7a59717a98c62a
Aurelien <aurelien@ziller.lu>
2020-04-18 15:30:26 +0200

    Formatted output

--- Auchan-Drive-Bot.py
+++ Auchan-Drive-Bot.py
@@ -42,12 +42,9 @@ def getMessageContent(location, available):
         return None
     elif isinstance(available, str):
         translate = [dateTranslate[word] if word in dateTranslate.keys() else word for word in available.split(" ")]
-        translate.insert(1,',')
-        translate.insert(2, ' ')
-        translate.insert(4, ' ')
-        translate.insert(6, ' ')
-        translate.insert(7, 'at ')
-        return "Slots available in {0}\n\nNext slot: {1}".format(location, "".join(translate))
+        translate[0] += ","
+        translate.insert(3, 'at')
+        return "Slots available in {0}\n\nNext slot: {1}".format(location, " ".join(translate))
 
 dateTranslate = {
     "lundi": "Monday",
